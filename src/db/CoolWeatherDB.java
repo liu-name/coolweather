64cea0b21531a27b541f4bd4b13d17ab0811420a
liuhongzhi <13621213112@163.com>
2015-02-05 17:31:55 +0800

    first commit.

---
+++ src/db/CoolWeatherDB.java
@@ -0,0 +1,26 @@
+package db;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class CoolWeatherDB {
+ //
+	/*
+	 * 数据库名
+	 */
+	public static final String DB_NAME = "cool_weather";
+	
+	/*
+	 * 数据库版本
+	 */
+	public static final int VERSION = 1;
+	 
+	private static CoolWeatherDB coolWeatherDB;
+	
+	private SQLiteDatabase db;
+
+
+
+
+
+
+}
